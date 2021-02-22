3342fce71751b02efd5701a1fe8606c90e201b2b
Bombaclath97 <e.bombardelli97@gmail.com>
2021-02-22 18:27:57 +0100

    Errore in loginservlet

--- HARDWARE_HOUSE/src/control/LoginServlet.java
+++ HARDWARE_HOUSE/src/control/LoginServlet.java
@@ -96,7 +96,6 @@ public class LoginServlet extends HttpServlet {
 				default: 
 					break;
 				}
-				response.addCookie(new Cookie("", ""));
 				response.setContentType("text/html");
 				response.sendRedirect((utente.getTipo() == "p" ? "home.jsp" : "adminpanel/homeadmin.jsp"));
 			}
